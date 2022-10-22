c153e057771b3b556a737eeb160c9f429edb255f
elliot-lyons <elliot243lyons@gmail.com>
2022-10-22 21:16:08 +0100

    Added first part of web app functionality.

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
 import System.IO
-import Text.Printf
+import Lib
+import Web.Spock
+import Web.Spock.Config
+import Control.Monad.Trans
+import Data.Semigroup ((<>))
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad (forM_)
+import Data.Text (Text, unpack, pack)
+import Data.IORef
+import Web.Spock.Lucid (lucid)
+import Lucid
 
-introduction :: IO ()
-introduction = do
-    printf "Haskell infix Calculator, Operators include \"+-*^le\", Note: l, e are log and exponent respectively, Input without Qutoes e.g. 2 + 3 instead of \"2 + 3\"\n"
-    printf "This calculator computes unary operators, (Does not work with exp and log even though they are technically numbers and not operators e.g. - e(4.2) is invalid\n" 
-    printf "Example of complex query with unary operators: - ( -3 + - -+ + 4 ) * l 10 - e(4.2) + 1.1234 * 34.343 / e 1.4\n"
+data MySession = EmptySession
+type Server a = SpockM () () ServerState a
+newtype ServerState = ServerState { expression :: IORef Expression}
+data Expression = Expression { contents :: Text }
 
-main :: IO ()
-main = do 
-    introduction
-    forever
-    where forever = do
-            printf "Please input an infix expression: "
-            hFlush stdout -- flush standard output explicitly in order to run getLine in the same line as putStr
-            infixExpr <- getLine
-            let splitInfixExrp = combineNum . removePlusNum . addZeroExponent . 
-                                 addZeroStringUnaryHeadPositiveOrNegative . removeUnaryHeadPositive . 
-                                 combineUnaryOperators . splitToList $ infixExpr -- split input to infix expression for evaluation and validation
-            let isInfixValid = infixValidator splitInfixExrp 
-            let infixCalculation = evaluatePostfix . infixToPostfix $ splitInfixExrp 
-            if isInfixValid
-                then do 
-                        case infixCalculation of -- only will ever be Just x
-                            (Just x)  -> printf "The answer is %.3g\n" x -- round to 3 decimal places and return x
-                else printf "Invalid, the answer cannot be calculated\n" -- Nothing
-            forever
+app :: Server ()
+app = do
+  get root $ do
+    expression' <- getState >>= (liftIO . readIORef . expression)
+    lucid $ do
+      h1_ "Haskell infix Calculator" 
+      p_ "Operators include \"+-*^le\", Note: l, e are log and exponent respectively, Input without Qutoes e.g. 2 + 3 instead of \"2 + 3\"\n"
+      p_ "This calculator computes unary operators, (Does not work with exp and log even though they are technically numbers and not operators e.g. - e(4.2) is invalid" 
+      p_ "Example of complex query with unary operators: - ( -3 + - -+ + 4 ) * l 10 - e(4.2) + 1.1234 * 34.343 / e 1.4"
+      form_ [method_ "post"] $ do
+        label_ $ do
+          "Mathematical Expression: "
+          textarea_ [name_ "Mathematical Expression"] ""
+        input_ [type_ "submit", value_ "calculate", name_ "result"]
+      form_ [method_ "text"] $ do
+        label_ $ do
+            output_ . toHtml . contents $ expression'
